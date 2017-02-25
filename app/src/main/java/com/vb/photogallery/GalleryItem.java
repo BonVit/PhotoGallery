f26b0b3013f211ebfeac2b6a53af8fc91aed4b0e
bonar <bonarv96@gmail.com>
2017-02-25 19:41:02 +0200

    Initial commit

---
+++ app/src/main/java/com/vb/photogallery/GalleryItem.java
@@ -0,0 +1,54 @@
+package com.vb.photogallery;
+
+import android.net.Uri;
+
+/**
+ * Created by bonar on 2/22/2017.
+ */
+
+public class GalleryItem {
+    private String mCaption;
+    private String mId;
+    private String mUrl;
+    private String mOwner;
+
+    public Uri getPhotoPageUri() {
+        return Uri.parse("http://www.flickr.com/photos/")
+                .buildUpon()
+                .appendPath(mOwner)
+                .appendPath(mId)
+                .build();
+    }
+
+    public String getOwner() {
+        return mOwner;
+    }
+
+    public void setOwner(String mOwner) {
+        this.mOwner = mOwner;
+    }
+
+    public String getCaption() {
+        return mCaption;
+    }
+
+    public void setCaption(String mCaption) {
+        this.mCaption = mCaption;
+    }
+
+    public String getId() {
+        return mId;
+    }
+
+    public void setId(String mId) {
+        this.mId = mId;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    public void setUrl(String mUrl) {
+        this.mUrl = mUrl;
+    }
+}
